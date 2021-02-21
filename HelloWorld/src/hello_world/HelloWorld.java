313bcc38715ab0372125a0f677e554a439ae40f0
dporahau <DPorahau@gomel.iba.by>
2021-02-21 15:35:31 +0300

    v3

--- HelloWorld/src/hello_world/HelloWorld.java
+++ HelloWorld/src/hello_world/HelloWorld.java
@@ -1,12 +1,14 @@
 package hello_world;
 
+import org.jetbrains.annotations.NotNull;
+
 class HelloWorld {
 
 	static int ageMultiply(int age){
 		return age * 3;
 	}
 	
-	public static void main(String[] args){ 
+	public static void main(String[] @NotNull args){
 		System.out.printf("Hello, I am %s my age is %s and if " +
 						"my age is multiplied by 3 than result is %d%n",
 				args[0], args[1], ageMultiply(Integer.parseInt(args[1])));
